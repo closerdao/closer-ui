60d55c0ebe0e3b128b9ae07b1434d7111e8d2cdb
samueldelesque <hello@samueldelesque.me>
2021-12-25 20:06:30 +0100

    Update imagery

--- pages/index.js
+++ pages/index.js
@@ -35,87 +35,86 @@ const getColors = () => {
 
 const index = () => {
   const [colors, setColors] = useState(getColors());
-  const [step, setStep] = useState(null);
-  const [showPassword, setShowPassword] = useState(false);
-  const [showWeb, enableWeb] = useState(false);
-  const [submitted, setSubmitted] = useState(false);
-  const refreshColors = () => {
-    const newColors = getColors();
-    setColors(newColors);
-  }
 
   return (
     <Layout>
       <Head>
         <title>Traditional Dream Factory | Regenerative coliving space in Alentejo, Portugal</title>
       </Head>
-      <div className={ showWeb ? 'password-entered' :'' }>
-        <main className="foreground m-3">
-          <section className="text-lg mb-20">
-            <h1 className="mt-24 italic mb-4 text-4xl font-display">A playground for living and creating together</h1>
+      <main className="foreground m-3">
+        <section className="text-lg mb-20">
+          <h1 className="mt-24 italic mb-4 text-4xl font-display">A playground for living and creating together</h1>
 
-            <p className="mb-4">We are living in an age of transformation. Our species has built technology & tools enabling it to reign over the natural kingdom, but in the process we have exhausted the natural resources of the planet, and altered it's ecology to the point of entering the 6th mass extinction event. Earth has lost 50% of its wildlife in the past 40 years<sup><a href="#footnotes">[1]</a></sup>, and if we do not radically change the way that we live over the next few decades we will lose most natural species, including ourselves.</p>
+          <p className="mb-4">We are living in an age of transformation. Our species has built technology & tools enabling it to reign over the natural kingdom, but in the process we have exhausted the natural resources of the planet, and altered it's ecology to the point of entering the 6th mass extinction event. Earth has lost 50% of its wildlife in the past 40 years<sup><a href="#footnotes">[1]</a></sup>, and if we do not radically change the way that we live over the next few decades we will lose most natural species, including ourselves.</p>
 
-            <p className="mb-4">We are here to prototype a better way. We believe that humans are kind by design <sup><a href="#footnotes">[2]</a></sup>, and we believe in creating a future that works for all life.</p>
+          <p className="mb-4">We are here to prototype a better way. We believe that humans are kind by design <sup><a href="#footnotes">[2]</a></sup>, and we believe in creating a future that works for all life.</p>
 
-            <p className="mb-4">We believe that our creativity is our greatest asset, and that we can channel our collective inteligence towards planetary regeneration.</p>
+          <p className="mb-4">We believe that our creativity is our greatest asset, and that we can channel our collective inteligence towards planetary regeneration.</p>
 
-            <p className="mb-4">We are here to re:imagine how we live together, to put into practice years of research in creating abundant food systems that work with nature, to retain water in our soils and avoid erosion and droughts, to create human living systems that leave a positive trace on it's environment.</p>
+          <p className="mb-4">We are here to re:imagine how we live together, to put into practice years of research in creating abundant food systems that work with nature, to retain water in our soils and avoid erosion and droughts, to create human living systems that leave a positive trace on it's environment.</p>
 
-            <p className="mb-4">This is not idealism or optimism. We are realists, and it is time that our modern societies wake up to the call. A new Earth is waiting. In this new planet, we are the stewards of the land. In this new world, we thrive by promoting biodiversity and being in the service of the greater ecosystem that we are a part of. We must re:learn to be part of Nature, and to respect its delicate cycles.</p>
+          <p className="mb-4">This is not idealism or optimism. We are realists, and it is time that our modern societies wake up to the call. A new Earth is waiting. In this new planet, we are the stewards of the land. In this new world, we thrive by promoting biodiversity and being in the service of the greater ecosystem that we are a part of. We must re:learn to be part of Nature, and to respect its delicate cycles.</p>
 
-            <p className="mb-4">We are engineers, artists, permaculturists, scientists, innovators, nomads.</p>
+          <p className="mb-4">We are engineers, artists, permaculturists, scientists, innovators, nomads.</p>
 
-            <hr className="my-4"/>
+          <hr className="my-8"/>
 
-            <p>Join the dreamers & the makers of tomorrow.</p>
-            <p>We are a regenerative living & creation collective based in Alentejo, Portugal.</p>
-            <p>Come build with us, play with us, grow with us.</p>
-            <br />
+          {/* <img className="w-full my-8" src="/images/backgrounds/tdf-courtyard.jpg" alt="TDF Courtyard" /> */}
 
-
-            {/* <p>Once upon a time, humanity was a free roaming herd of nomad gatherers.</p>
-            <p>Over the course of centuries, our path changed, with established global human systems increasingly capable of reigning over the natural kingdom.</p>
-            <p>We entered the anthropocene. The total weight of human made objects exceeded that of all other living beings combined.</p>
-            <p>We ran into some challenges - like climate change and political tensions.</p>
-            <p>What was really happening is that what did for a living and what "making a living" lost its sense of purpose.</p>
-            <hr className="my-4"/>
-            <p>A new reality emerges.</p>
-            <p>We feel empowered to unite, changing forever the way we live, the way we work, the way we play.</p>
-            <p>We enter a new age of abundance.</p>
-            <p>A playful age.</p>
-            <p>An age where our creativity is our greatest asset, and our collective inteligence regenerates the planet.</p>
-            <hr className="my-4"/>
-            <p>Join the dreamers & the makers of tomorrow.</p>
-            <p>We are a regenerative living & creation collective based in Alentejo, Portugal.</p>
-            <p>Come build with us, play with us, grow with us.</p>
-            <br /> */}
-            <p className="mb-4">
-              <Link href="/signup">
-                <a type="submit" className="button px-4 py-1 mr-2 mb-2 rounded-full bg-black hover:bg-pink-500 text-white font-bold">
-                  Become Member
-                </a>
-              </Link>
-              <Link href="https://docs.google.com/document/d/177JkHCy0AhplsaEEYpFHBsiI6d4uLk0TgURSKfBIewE/mobilebasic">
-                <a type="submit" className="button px-4 py-1 mr-2 mb-2 rounded-full bg-black hover:bg-pink-500 text-white font-bold" rel="nofollow noreferrer" target="_blank">
-                  Read Pink Paper
-                </a>
-              </Link>
-            </p>
+          <p>Join the dreamers & the makers of tomorrow.</p>
+          <p>We are a regenerative living & creation collective based in Alentejo, Portugal.</p>
+          <p>Come build with us, play with us, grow with us.</p>
+          <br />
 
 
+          {/* <p>Once upon a time, humanity was a free roaming herd of nomad gatherers.</p>
+          <p>Over the course of centuries, our path changed, with established global human systems increasingly capable of reigning over the natural kingdom.</p>
+          <p>We entered the anthropocene. The total weight of human made objects exceeded that of all other living beings combined.</p>
+          <p>We ran into some challenges - like climate change and political tensions.</p>
+          <p>What was really happening is that what did for a living and what "making a living" lost its sense of purpose.</p>
+          <hr className="my-4"/>
+          <p>A new reality emerges.</p>
+          <p>We feel empowered to unite, changing forever the way we live, the way we work, the way we play.</p>
+          <p>We enter a new age of abundance.</p>
+          <p>A playful age.</p>
+          <p>An age where our creativity is our greatest asset, and our collective inteligence regenerates the planet.</p>
+          <hr className="my-4"/>
+          <p>Join the dreamers & the makers of tomorrow.</p>
+          <p>We are a regenerative living & creation collective based in Alentejo, Portugal.</p>
+          <p>Come build with us, play with us, grow with us.</p>
+          <br /> */}
+          <p className="mb-4">
+            <Link href="/signup">
+              <a type="submit" className="button px-4 py-1 mr-2 mb-2 rounded-full bg-black hover:bg-pink-500 text-white font-bold">
+                Become Member
+              </a>
+            </Link>
+            <Link href="https://docs.google.com/document/d/177JkHCy0AhplsaEEYpFHBsiI6d4uLk0TgURSKfBIewE/mobilebasic">
+              <a type="submit" className="button px-4 py-1 mr-2 mb-2 rounded-full bg-black hover:bg-pink-500 text-white font-bold" rel="nofollow noreferrer" target="_blank">
+                Read Pink Paper
+              </a>
+            </Link>
+          </p>
 
+          <div id="footnotes" className="mt-8 italic text-sm">
+            <ol className="list-decimal pl-6">
+              <li><p><a href="https://www.theguardian.com/environment/2014/sep/29/earth-lost-50-wildlife-in-40-years-wwf" target="_blank" rel="noreferrer nofollow">Earth has lost half of its wildlife in the past 40 years, says WWF</a></p></li>
+              <li>Human kind - a hopeful history, Rutger Bregman.</li>
+            </ol>
+          </div>
+        </section>
 
-            <div id="footnotes" className="mt-8 italic text-sm">
-              <ol className="list-decimal pl-6">
-                <li><p><a href="https://www.theguardian.com/environment/2014/sep/29/earth-lost-50-wildlife-in-40-years-wwf" target="_blank" rel="noreferrer nofollow">Earth has lost half of its wildlife in the past 40 years, says WWF</a></p></li>
-                <li>Human kind - a hopeful history, Rutger Bregman.</li>
-              </ol>
-            </div>
-          </section>
-          <Timeline />
-        </main>
-      </div>
+        <section className="my-8">
+          <p>
+            <figure>
+              <img className="w-full" src="/images/architecture/tdf-plan.png" alt="Architectural plans" />
+              <figcaption>Architectural plans: 14 suites, 3 studios, 1 house, natural swimming pool, coworking, restaurant and event space - <a href="https://cruatelier.pt/" target="_blank">CRU architects</a>.</figcaption>
+            </figure>
+          </p>
+        </section>
+
+        <Timeline />
+      </main>
     </Layout>
   );
 }
