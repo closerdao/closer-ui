c7ea9bb91922b17e6ffba66d1484e5b1b02c633b
Vladislav Sorokin <sorokinvj@gmail.com>
2022-11-27 19:47:34 +0000

    feat: show new booking only for auth users

--- pages/bookings/new/index.js
+++ pages/bookings/new/index.js
@@ -1,16 +1,26 @@
 import { useRouter } from 'next/router';
 
 import { useEffect } from 'react';
+import React from 'react';
 
+import PageNotAllowed from '../../401';
+import { useAuth } from '../../../contexts/auth';
 import { BOOKING_PATHS } from '../../../utils/const';
 
 const NewBooking = () => {
   const router = useRouter();
+  const { isAuthenticated } = useAuth();
   const [firstStepUrl] = BOOKING_PATHS;
 
   useEffect(() => {
-    router.push(firstStepUrl);
-  }, []);
+    if (isAuthenticated) {
+      router.push(firstStepUrl);
+    }
+  }, [isAuthenticated]);
+
+  if (!isAuthenticated) {
+    return <PageNotAllowed />;
+  }
 
   return null;
 };
