b3c59aa795677b2086b469d440a924b1da676e66
Vladislav Sorokin <sorokinvj@gmail.com>
2022-12-14 19:51:42 +0100

    feat: booking card with call to actions

--- components/BookingListPreview.js
+++ components/BookingListPreview.js
@@ -47,17 +47,17 @@ const BookingListPreview = ({ booking: bookingMapItem, listingName }) => {
     rentalToken,
     rentalFiat,
     utilityFiat,
-    description,
   } = bookingMapItem.toJS();
-  console.log(bookingMapItem.toJS());
   const { user } = useAuth();
   const { platform } = usePlatform();
-  const now = dayjs();
   const startFormatted = dayjs(start).format('DD/MM/YYYY');
   const endFormatted = dayjs(end).format('DD/MM/YYYY');
-  const isBookingCancelable = now.isBefore(start);
   const createdFormatted = dayjs(created).format('DD/MM/YYYY - HH:mm:A');
   const isNotPaid = status === 'open';
+
+  const isBookingCancelable =
+    createdBy === user._id &&
+    (status === 'open' || status === 'pending' || status === 'confirmed');
 
   const confirmBooking = async () => {
     await platform.bookings.confirm(_id);
@@ -67,76 +67,97 @@ const BookingListPreview = ({ booking: bookingMapItem, listingName }) => {
   };
 
   return (
-    <div className="max-w-sm bg-white rounded-lg p-4 shadow-xl flex flex-col md:basis-5/12 md:flex-1 gap-3">
-      <div>
-        <p className="card-feature">
-          {__('booking_card_id')}
-          {_id}
-        </p>
-        <p className="card-feature">{createdFormatted}</p>
+    <div className="max-w-sm bg-white rounded-lg p-4 shadow-xl flex flex-col md:basis-5/12 md:flex-1">
+      <div className="flex flex-col gap-3">
+        <div>
+          <p className="card-feature">
+            {__('booking_card_id')}
+            {_id}
+          </p>
+          <p className="card-feature">{createdFormatted}</p>
+        </div>
+        <div>
+          <p className="card-feature">{__('booking_card_status')}</p>
+          <p
+            className={`capitalize opacity-100 text-base ${statusColor[status]}`}
+          >
+            {status}
+          </p>
+        </div>
+        <div>
+          <p className="card-feature">{__('booking_card_message')}</p>
+          <p>{getStatusText(status, updated)}</p>
+        </div>
+        <div>
+          <p className="card-feature">{__('booking_card_guests')}</p>
+          <p>{adults}</p>
+        </div>
+        <div>
+          <p className="card-feature">{__('booking_card_checkin')}</p>
+          <p>{startFormatted}</p>
+        </div>
+        <div>
+          <p className="card-feature">{__('booking_card_checkout')}</p>
+          <p>{endFormatted}</p>
+        </div>
+        <div>
+          <p className="card-feature">{__('booking_card_type')}</p>
+          <p>{listingName}</p>
+        </div>
+        <div>
+          <p className="card-feature">
+            {__('booking_card_payment_accomodation')}
+          </p>
+          <p>
+            {useTokens ? priceFormat(rentalToken) : priceFormat(rentalFiat)}{' '}
+            {isNotPaid && (
+              <span className="text-failure">{__('booking_card_unpaid')}</span>
+            )}
+          </p>
+        </div>
+        <div>
+          <p className="card-feature">{__('booking_card_payment_utility')}</p>
+          <p>
+            {priceFormat(utilityFiat)}{' '}
+            {isNotPaid && (
+              <span className="text-failure">{__('booking_card_unpaid')}</span>
+            )}
+          </p>
+        </div>
       </div>
-      <div>
-        <p className="card-feature">{__('booking_card_status')}</p>
-        <p
-          className={`capitalize opacity-100 text-base ${statusColor[status]}`}
-        >
-          {status}
-        </p>
-      </div>
-      <div>
-        <p className="card-feature">{__('booking_card_message')}</p>
-        <p>{getStatusText(status, updated)}</p>
-      </div>
-      <div>
-        <p className="card-feature">{__('booking_card_guests')}</p>
-        <p>{adults}</p>
-      </div>
-      <div>
-        <p className="card-feature">{__('booking_card_checkin')}</p>
-        <p>{startFormatted}</p>
-      </div>
-      <div>
-        <p className="card-feature">{__('booking_card_checkout')}</p>
-        <p>{endFormatted}</p>
-      </div>
-      <div>
-        <p className="card-feature">{__('booking_card_type')}</p>
-        <p>{listingName}</p>
-      </div>
-      <div>
-        <p className="card-feature">
-          {__('booking_card_payment_accomodation')}
-        </p>
-        <p>
-          {useTokens ? priceFormat(rentalToken) : priceFormat(rentalFiat)}{' '}
-          {isNotPaid && (
-            <span className="text-failure">{__('booking_card_unpaid')}</span>
-          )}
-        </p>
-      </div>
-      <div>
-        <p className="card-feature">{__('booking_card_payment_utility')}</p>
-        <p>
-          {priceFormat(utilityFiat)}{' '}
-          {isNotPaid && (
-            <span className="text-failure">{__('booking_card_unpaid')}</span>
-          )}
-        </p>
-      </div>
-      {description && (
-        <p>
-          {description.slice(0, 120)}
-          {description.length > 120 && '...'}
-        </p>
-      )}
-      <div className="my-2">
-        {user && user._id === createdBy && (
+
+      <div className="mt-8 flex flex-col gap-4">
+        {status === 'checked-in' && (
+          <Link passHref href="">
+            <a>
+              <button className="btn w-full uppercase ">
+                {__('booking_card_join_chat_button')}
+              </button>
+            </a>
+          </Link>
+        )}
+        {status === 'checked-out' && (
+          <Link passHref href="">
+            <a>
+              <button className="btn w-full uppercase ">
+                {__('booking_card_feedback_button')}
+              </button>
+            </a>
+          </Link>
+        )}
+        {status === 'open' && (
+          <Link passHref href={`/bookings/${_id}/checkout`}>
+            <a>
+              <button className="btn w-full uppercase ">
+                {__('booking_card_checkout_button')}
+              </button>
+            </a>
+          </Link>
+        )}
+        {user && isBookingCancelable && (
           <Link passHref href={`/bookings/${_id}/cancel`}>
             <a>
-              <button
-                disabled={!isBookingCancelable}
-                className="btn disabled:text-gray-400 disabled:border-gray-400 disabled:cursor-not-allowed"
-              >
+              <button className="btn w-full uppercase">
                 {__('booking_cancel_button')}
               </button>
             </a>
@@ -148,7 +169,7 @@ const BookingListPreview = ({ booking: bookingMapItem, listingName }) => {
             <>
               {status === 'pending' && (
                 <button
-                  className="btn w-full mb-2 text-center"
+                  className="btn w-full uppercase"
                   onClick={(e) => confirmBooking(e)}
                 >
                   {__('booking_confirm_button')}
@@ -156,15 +177,17 @@ const BookingListPreview = ({ booking: bookingMapItem, listingName }) => {
               )}
               {status === 'pending' && (
                 <button
-                  className="btn w-full mb-2 text-center"
+                  className="btn w-full uppercase"
                   onClick={(e) => rejectBooking(e)}
                 >
                   {__('booking_reject_button')}
                 </button>
               )}
               <Link passHref href={`/members/${createdBy}`}>
-                <a className="btn w-full text-center">
-                  {__('booking_view_profile')}
+                <a>
+                  <button className="btn w-full uppercase">
+                    {__('booking_view_profile')}
+                  </button>
                 </a>
               </Link>
             </>
