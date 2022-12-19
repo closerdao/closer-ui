14ff05403194e7bcf9c44f36d2208a54d08c1ba4
Vladislav Sorokin <sorokinvj@gmail.com>
2022-12-19 21:35:11 +0000

    feat: add user photo to nav

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 import React, { useEffect, useState } from 'react';
@@ -7,6 +8,7 @@ import GuestMenu from './GuestMenu';
 import Logo from './Logo';
 import MemberMenu from './MemberMenu';
 import Menu from './MenuContainer';
+import ProfilePhoto from './ProfilePhoto';
 
 const Navigation = () => {
   const [navOpen, setNavOpen] = useState(false);
@@ -14,26 +16,42 @@ const Navigation = () => {
     setNavOpen((isOpen) => !isOpen);
   };
 
+  const closeNav = () => {
+    setNavOpen(false);
+  };
+
   const router = useRouter();
   useEffect(() => {
-    router.events.on('routeChangeComplete', toggleNav);
-    router.events.on('routeChangeError', toggleNav);
+    router.events.on('routeChangeComplete', closeNav);
+    router.events.on('routeChangeError', closeNav);
 
     return () => {
-      router.events.off('routeChangeComplete', toggleNav);
-      router.events.off('routeChangeError', toggleNav);
+      router.events.off('routeChangeComplete', closeNav);
+      router.events.off('routeChangeError', closeNav);
     };
   }, [router]);
 
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, user } = useAuth();
 
   return (
     <div className="NavContainer pt-20 md:pt-0 relative z-20">
       <div className="main-content h-20 fixed z-50 top-0 left-0 right-0 shadow-sm md:relative  flex justify-between items-center">
         <Logo />
-        <Menu isOpen={navOpen} toggleNav={toggleNav}>
-          {isAuthenticated ? <MemberMenu /> : <GuestMenu />}
-        </Menu>
+        <div className="flex gap-4">
+          {isAuthenticated && (
+            <Link href={`/members/${user?.slug}`} passsHref>
+              <a
+                title="View profile"
+                className="hidden md:flex md:flex-row items-center z-0"
+              >
+                <ProfilePhoto user={user} />
+              </a>
+            </Link>
+          )}
+          <Menu isOpen={navOpen} toggleNav={toggleNav}>
+            {isAuthenticated ? <MemberMenu /> : <GuestMenu />}
+          </Menu>
+        </div>
       </div>
     </div>
   );
