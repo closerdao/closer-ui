e0620f825e89befe912d87218576d1434343e214
Vladislav Sorokin <sorokinvj@gmail.com>
2022-12-20 17:28:36 +0000

    fix: popup layout

--- components/CookiePopup.js
+++ components/CookiePopup.js
@@ -14,18 +14,23 @@ const GdprCompliancePopup = () => {
   if (acceptedCookieWarning) return null;
 
   return (
-    <div className="absolute bottom-0 w-full flex flex-col p-4 bg-white md:flex-row md:px-8 shadow-2xl">
-      <div className="md:mr-14">
-        <h1 className="text-2xl font-normal">
-          <span className="mr-1">🍪</span>
-          <span>{__('gdpr-popup-title')}</span>
-        </h1>
-        <p className="mt-2">{__('gdpr-popup-description')}</p>
-      </div>
-      <div className="my-4 flex flex-col gap-4 md:flex-row md:flex-1">
-        <button className="btn uppercase md:basis-1/2" onClick={onAccept}>
-          {__('gdpr-popup-button-accept')}
-        </button>
+    <div className="absolute bottom-0 w-full bg-white shadow-2xl">
+      <div className="main-content p-4 flex flex-col items-center md:flex-row md:px-8">
+        <div className="md:mr-14">
+          <h1 className="text-2xl font-normal">
+            <span className="mr-1">🍪</span>
+            <span>{__('gdpr-popup-title')}</span>
+          </h1>
+          <p className="mt-2">{__('gdpr-popup-description')}</p>
+        </div>
+        <div className="w-full my-4 flex flex-col gap-4 md:flex-row md:flex-1">
+          <button
+            className="btn uppercase w-full md:basis-1/2 h-fit"
+            onClick={onAccept}
+          >
+            {__('gdpr-popup-button-accept')}
+          </button>
+        </div>
       </div>
     </div>
   );
