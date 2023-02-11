5c0335669c22ecc4226f0b04bdc6f1361aac4734
Vladislav Sorokin <sorokinvj@gmail.com>
2023-02-11 14:57:35 +0000

    fix: init analytics

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,17 @@
 import { useEffect } from 'react';
 
+import { GA_ANALYTICS } from '../config';
 import { initAnalytics, trackPageView } from './Analytics';
 
 let GA_INITIALIZED;
 
 const Layout = ({ children }) => {
   useEffect(() => {
-    if (!GA_INITIALIZED) {
+    if (!GA_INITIALIZED && GA_ANALYTICS) {
       initAnalytics();
       GA_INITIALIZED = true;
+      trackPageView();
     }
-    trackPageView();
   }, []);
 
   return children;
