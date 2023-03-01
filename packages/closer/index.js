ccd4ab0cae1e2fc7c2b400f7ba13322b917eb13f
Vladislav Sorokin <sorokinvj@gmail.com>
2023-03-01 22:22:01 +0000

    feat: turn on other pages in tdf

--- packages/closer/index.js
+++ packages/closer/index.js
@@ -17,6 +17,25 @@ export { default as LoginPage } from './pages/login';
 export { default as ForgotPasswordPage } from './pages/login/forgot-password';
 export { default as SetPasswordPage } from './pages/login/set-password';
 
+export { default as BookingsPage } from './pages/bookings/index';
+export { default as BookingsRequestsPage } from './pages/bookings/requests';
+export { default as BookingPage } from './pages/bookings/[slug]/index';
+export { default as BookingCancelPage } from './pages/bookings/[slug]/cancel';
+export { default as BookingCheckoutPage } from './pages/bookings/[slug]/checkout';
+export { default as BookingConfirmationPage } from './pages/bookings/[slug]/confirmation';
+export { default as BookingQuestionsPage } from './pages/bookings/[slug]/questions';
+export { default as BookingSummaryPage } from './pages/members';
+export { default as CreateBookingPage } from './pages/bookings/create/index';
+export { default as CreateBookingAccomodationPage } from './pages/bookings/create/accomodation';
+export { default as CreateBookingDatesPage } from './pages/bookings/create/dates';
+export { default as EditBookingPage } from './pages/bookings/edit/[slug]';
+export { default as MembersPage } from './pages/members/index';
+export { default as MemberPage } from './pages/members/[slug]';
+export { default as TicketPage } from './pages/tickets/[slug]/index';
+export { default as TicketsInvoicePage } from './pages/tickets/[slug]/invoice';
+export { default as TokenSalePage } from './pages/token-sale';
+export { default as TokenSaleReferralPage } from './pages/token-sale/[referralId]';
+
 // Components
 export { default as Footer } from './components/Footer';
 export { default as Navigation } from './components/Navigation';
