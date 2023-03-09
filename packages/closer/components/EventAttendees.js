df307fd175da391e29bf8f3feece81ab7eb0abe4
closerearth <closer.earth@protonmail.com>
2023-03-09 13:13:48 +0000

    Remove double count check

--- packages/closer/components/EventAttendees.js
+++ packages/closer/components/EventAttendees.js
@@ -5,7 +5,7 @@ import React from 'react';
 import dayjs from 'dayjs';
 
 import ProfilePhoto from './ProfilePhoto';
-const TicketCounter = ({ count }) => count > 0 && (
+const TicketCounter = ({ count }) => (
   <div className="z-10 w-9 h-9 inline-flex justify-center items-center text-center rounded-full overflow-hidden bg-black text-white text-sm">
     +{Math.min(count, 99)}
   </div>
