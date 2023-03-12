4dad7968481d58e843d18330875e9757613d8ff8
Vladislav Sorokin <sorokinvj@gmail.com>
2023-03-12 16:09:26 +0000

    fix: guest view token sale

--- packages/closer/components/TokenSaleHeader.js
+++ packages/closer/components/TokenSaleHeader.js
@@ -14,6 +14,7 @@ const TokenSaleHeader = ({ title, description, saleDate }) => {
   const redirectToTokenSale = () => {
     router.push('/token-sale');
   };
+  console.log('header', title, description);
 
   if (!componentHasMounted || !saleDate) {
     // this prevents hydration errors
