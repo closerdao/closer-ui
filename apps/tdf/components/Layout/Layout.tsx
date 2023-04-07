3eab13fdc8e637ac3d40c6765246666ef573db96
closerearth <closer.earth@protonmail.com>
2023-04-08 00:36:21 +0100

    Fix top margin

--- apps/tdf/components/Layout/Layout.tsx
+++ apps/tdf/components/Layout/Layout.tsx
@@ -6,7 +6,7 @@ import { Navigation } from 'closer';
 
 export const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="flex-1 flex flex-col relative mx-auto w-full">
+    <div className="flex-1 flex flex-col relative mx-auto mt-20 w-full">
       <Navigation />
       <div className="p-4 overflow-hidden">{children}</div>
       <Footer />
