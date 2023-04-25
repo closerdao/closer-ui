4567890d1c1cd956943308d9263ad0878b93256b
VV <vashnev@gmail.com>
2023-04-25 19:38:59 +0600

    renamed Accommodation to Stay in booking types

--- packages/closer/utils/helpers.js
+++ packages/closer/utils/helpers.js
@@ -286,7 +286,7 @@ export const getBookingType = (eventId, volunteerId) => {
   } else if (volunteerId) {
     bookingType = 'volunteer';
   } else {
-    bookingType = 'accomodation';
+    bookingType = 'stay';
   }
   return bookingType;
 };
