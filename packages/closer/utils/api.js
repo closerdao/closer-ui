15bd62fe4fae49310d45296a28016a3a36c65e31
closerearth <closer.earth@protonmail.com>
2023-04-25 16:19:02 +0100

    Add authorized event loading

--- packages/closer/utils/api.js
+++ packages/closer/utils/api.js
@@ -6,6 +6,7 @@ export const cdn = process.env.NEXT_PUBLIC_CDN_URL;
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
+  withCredentials: true
 });
 
 if (process.env.NEXT_PUBLIC_LOG_REQUESTS === 'true') {
