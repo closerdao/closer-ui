60bc9ebfde1910b8e926c1dcdf8177705729d213
VV <vashnev@gmail.com>
2023-06-04 18:49:20 +0600

    fix back button bug in event/volunteer booking

--- packages/closer/pages/bookings/[slug]/questions.tsx
+++ packages/closer/pages/bookings/[slug]/questions.tsx
@@ -8,6 +8,7 @@ import QuestionnaireItem from '../../../components/QuestionnaireItem';
 import Button from '../../../components/ui/Button';
 import ProgressBar from '../../../components/ui/ProgressBar';
 
+import dayjs from 'dayjs';
 import { ParsedUrlQuery } from 'querystring';
 
 import PageNotAllowed from '../../401';
@@ -35,6 +36,7 @@ interface Props extends BaseBookingParams {
 
 const Questionnaire = ({ eventQuestions, booking, error }: Props) => {
   const router = useRouter();
+  const { goBack } = router.query;
   const { isAuthenticated } = useAuth();
   const questions: Question[] = prepareQuestions(eventQuestions);
 
@@ -63,6 +65,10 @@ const Questionnaire = ({ eventQuestions, booking, error }: Props) => {
     //this is a temporary solution to redirect to summary page if there are no questions
     //once we have questions from user profile integrated we should remove this
     if (!questions?.length) {
+      if (goBack === 'true') {
+        resetBooking();
+        return;
+      }
       router.push(`/bookings/${booking._id}/summary`);
     }
   }, []);
@@ -91,7 +97,29 @@ const Questionnaire = ({ eventQuestions, booking, error }: Props) => {
   };
 
   const resetBooking = () => {
-    router.push('/bookings/create');
+    if (booking.eventId) {
+      router.push(
+        `/bookings/create/dates?eventId=${booking.eventId}&start=${dayjs(
+          booking.start,
+        ).format('YYYY-MM-DD')}&end=${dayjs(booking.end).format('YYYY-MM-DD')}`,
+      );
+      return;
+    }
+    if (booking.volunteerId) {
+      router.push(
+        `/bookings/create/dates?volunteerId=${
+          booking.volunteerId
+        }&start=${dayjs(booking.start).format('YYYY-MM-DD')}&end=${dayjs(
+          booking.end,
+        ).format('YYYY-MM-DD')}`,
+      );
+      return;
+    }
+    router.push(
+      `/bookings/create/dates?start=${dayjs(booking.start).format(
+        'YYYY-MM-DD',
+      )}&end=${dayjs(booking.end).format('YYYY-MM-DD')}`,
+    );
   };
 
   const getAnswer = (
@@ -163,7 +191,11 @@ Questionnaire.getInitialProps = async ({
       booking.eventId && (await api.get(`/event/${booking.eventId}`));
     const event = optionalEvent?.data?.results;
 
-    return { booking, eventQuestions: event?.fields, error: null };
+    return {
+      booking,
+      eventQuestions: event?.fields,
+      error: null,
+    };
   } catch (err) {
     return {
       error: parseMessageFromError(err),
