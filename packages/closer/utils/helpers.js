ac06dc657ecf94cdc6d9ca231a107cb92e8cdc1c
VV <vashnev@gmail.com>
2023-07-03 13:21:22 +0600

    added address validation to token sale kyc

--- packages/closer/utils/helpers.js
+++ packages/closer/utils/helpers.js
@@ -331,12 +331,22 @@ const validationPatterns = {
   email: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
   phone: /^[0-9()+-\s]{5,}$/,
   taxNo: /^$|^[0-9()\-\s]{6,}$/,
-} ;
+  swissAddress: /Switzerland/i,
+  usAddress: /\s\b(us|usa|united\sstates)\b/,
+};
 
-export const isInputValid = (value, validation) => {
+const doesRegexMatch = (value, validation) => {
   const pattern = validationPatterns[validation];
   if (pattern) {
     return pattern.test(value);
   }
   return true;
+};
+
+export const isInputValid = (value, validation) => {
+  return doesRegexMatch(value, validation);
+};
+
+export const doesAddressMatchPattern = (value, validation) => {
+  return doesRegexMatch(value, validation);
 };
