3ab1f536a5bed9358c8497d0dc656ff0f9feb268
VV <vashnev@gmail.com>
2023-08-24 22:13:25 +0600

    fix fractional token conversion

--- packages/closer/components/Wallet.js
+++ packages/closer/components/Wallet.js
@@ -23,11 +23,11 @@ const Wallet = () => {
         <div className="flex flex-col gap-2 mt-4">
           <div className="flex justify-between items-center">
             <p>{__('wallet_tdf')}</p>
-            <p className="font-bold">{balanceTotal.toFixed(2)}</p>
+            <p className="font-bold">{balanceTotal}</p>
           </div>
           <div className="flex justify-between items-center">
             <p>{__('wallet_tdf_available')}</p>
-            <p className="font-bold">{balanceAvailable.toFixed(2)}</p>
+            <p className="font-bold">{balanceAvailable}</p>
           </div>
           <div className="flex justify-between items-center">
             <p>{__('wallet_pop')}</p>
