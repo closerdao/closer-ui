05cbfc3dbd70bcf381af7fe6abd2c77cd64d06b1
Sam <closer.earth@protonmail.com>
2024-01-14 21:59:11 +0100

    Add art faire page

--- apps/tdf/pages/_app.tsx
+++ apps/tdf/pages/_app.tsx
@@ -49,7 +49,7 @@ const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
       </Head>
 
       <Script
