bd78f968a093f22f063d9784db2aa0e9b49068ed
VV <vashnev@gmail.com>
2024-02-20 20:50:12 +0600

    hotfix: blinking logo

--- packages/closer/components/Logo.tsx
+++ packages/closer/components/Logo.tsx
@@ -6,16 +6,22 @@ import { useConfig } from '../hooks/useConfig';
 
 const Logo: FC = () => {
   const config = useConfig();
-  const { LOGO_HEADER, PLATFORM_NAME } = config || {};
+  const { LOGO_HEADER, PLATFORM_NAME } = config;
 
-  return (
+  console.log('config=', config);
+
+  return PLATFORM_NAME !== '[object Object]' ? (
     <Link href="/" className="block">
       {LOGO_HEADER ? (
         <img src={LOGO_HEADER} alt={PLATFORM_NAME} className="h-12" />
       ) : (
-        <div className="font-bold text-xl">{PLATFORM_NAME}</div>
+        <div className="font-normal text-4xl tracking-wider">
+          {PLATFORM_NAME}
+        </div>
       )}
     </Link>
+  ) : (
+    <div></div>
   );
 };
 
