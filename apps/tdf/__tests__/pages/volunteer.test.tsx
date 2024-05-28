c44a44cd7589c0661eba7a5b7238c790d6b9cf99
VV <vashnev@gmail.com>
2024-05-28 16:07:55 +0500

    test: fix volunteer test

--- apps/tdf/__tests__/pages/volunteer.test.tsx
+++ apps/tdf/__tests__/pages/volunteer.test.tsx
@@ -12,13 +12,19 @@ import VolunteerPage from '../../pages/volunteer';
 import { volunteerEventMock } from '../mocks/volunteerEvent';
 
 describe('Volunteer', () => {
+  beforeEach(() => {
+    // Mock environment variable
+    process.env = Object.assign(process.env, { NEXT_PUBLIC_FEATURE_VOLUNTEERING: 'true' });
+  });
   it('should render and have proper title', () => {
+    const volunteerConfigMock = { enabled: true };
+
     const eventListMock = [volunteerEventMock];
     render(
       <ConfigProvider config={{ ...closerConfig, ...blockchainConfig }}>
         <AuthProvider>
           <PlatformProvider>
-            <VolunteerPage opportunities={eventListMock} />
+            <VolunteerPage opportunities={eventListMock} volunteerConfig={volunteerConfigMock} />
           </PlatformProvider>
         </AuthProvider>
       </ConfigProvider>,
