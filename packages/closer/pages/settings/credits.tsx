30858b0a56333f29a014911f86e80679157c313b
VV <vashnev@gmail.com>
2024-07-16 19:20:04 +0500

    hotfix: moos vybes copy

--- packages/closer/pages/settings/credits.tsx
+++ packages/closer/pages/settings/credits.tsx
@@ -8,10 +8,11 @@ import Heading from '../../components/ui/Heading';
 import { NextPageContext } from 'next';
 import { useTranslations } from 'next-intl';
 
+import { Page404, api } from '../..';
 import { usePlatform } from '../../contexts/platform';
 import { useConfig } from '../../hooks/useConfig';
+import { parseMessageFromError } from '../../utils/common';
 import { loadLocaleData } from '../../utils/locale.helpers';
-import PageNotFound from '../not-found';
 
 const CreditsPage = () => {
   const t = useTranslations();
@@ -19,30 +20,31 @@ const CreditsPage = () => {
   const { platform }: any = usePlatform();
 
   if (process.env.NEXT_PUBLIC_FEATURE_CARROTS !== 'true') {
-    return <PageNotFound error="" />;
+    return <Page404 error="" />;
   }
 
   return (
     <>
       <Head>
-        <title>{APP_NAME && t('carrots_heading')}</title>
+        <title>{ t('carrots_heading')}</title>
       </Head>
       <div className="max-w-screen-sm mx-auto md:p-8 h-full main-content w-full flex flex-col gap-12  min-h-screen py-2">
         <div className="bg-accent-light rounded-md p-6 flex flex-wrap content-center justify-center">
           <Heading level={1} className="flex justify-center flex-wrap">
             <div className="text-6xl w-full flex justify-center">
-              {t('carrots_balance')}
+              { t('carrots_balance')}
             </div>
-            {t('carrots_heading')}
+            { t('carrots_heading')}
           </Heading>
           <Heading
             level={2}
             className="p-4 text-xl text-center font-normal w-full"
           >
             {' '}
-            {t('carrots_subheading')}
+            { t('carrots_subheading')}
           </Heading>
         </div>
+
         <Card>
           <div className="flex">
             <Heading level={3} className="w-1/2">
@@ -50,28 +52,36 @@ const CreditsPage = () => {
             </Heading>
             <Heading level={3} className="w-1/2 text-right">
               {(platform.carrots.findBalance('carrots') || 0).toFixed(2)}{' '}
-              {t('carrots_balance')}
+              { t('carrots_balance')}
             </Heading>
           </div>
         </Card>
+
         {APP_NAME && APP_NAME.toLowerCase() === 'moos' && (
-          <Heading level={3}>{t('carrots_subheading_what_are')}</Heading>
+          <Heading level={3}>
+            {t('carrots_subheading_what_are')}
+          </Heading>
         )}
+
         {APP_NAME && APP_NAME.toLowerCase() === 'moos' && (
           <div>
-            <p className="mb-4">{t('carrots_what_are_1')}</p> 
+            <p className="mb-4">{t('carrots_what_are_1')}</p>
             <p className="mb-4">{t('carrots_what_are_2')}</p>
           </div>
         )}
-        <Heading level={3}>{t('carrots_subheading_what')}</Heading>
+
+        <Heading level={3}>
+          {APP_NAME && t('carrots_subheading_what')}
+        </Heading>
+
         <div>
-          <p className="mb-4">{t('carrots_what_1')}</p>
-          <p className="mb-4">{t('carrots_what_2')}</p>
+          <p className="mb-4">{APP_NAME && t('carrots_what_1')}</p>
+          <p className="mb-4">{APP_NAME && t('carrots_what_2')}</p>
           {APP_NAME && APP_NAME.toLowerCase() === 'moos' && (
             <p className="mb-4">{t('carrots_what_2_5')}</p>
           )}
-          <p className="mb-4">{t('carrots_what_3')}</p>
-          <p className="mb-4">{t('carrots_what_4')}</p>
+          <p className="mb-4">{APP_NAME && t('carrots_what_3')}</p>
+          <p className="mb-4">{APP_NAME && t('carrots_what_4')}</p>
 
           {APP_NAME && APP_NAME.toLowerCase() === 'moos' && (
             <>
@@ -80,13 +90,13 @@ const CreditsPage = () => {
             </>
           )}
         </div>
+
         <Heading level={3}>{t('carrots_subheading_where')}</Heading>
+
         <div>
           <p className="mb-4">{t('carrots_where_1')}</p>
           <Profile isDemo={true} />
         </div>
-
-        <Heading level={3}>{t('carrots_subheading_how_to_use')}</Heading>
 
         {APP_NAME && (
           <>
@@ -108,6 +118,7 @@ const CreditsPage = () => {
         )}
 
         <RedeemCredits isDemo={true} />
+
         {/* {APP_NAME && APP_NAME.toLowerCase() !== 'moos' && (
           <>
             <Heading level={3}>{t('carrots_subheading_how_to_earn')}</Heading>
@@ -127,13 +138,27 @@ const CreditsPage = () => {
             </Heading>
 
             <div>
-              <p className="mb-4">{t('carrots_additional_guidelines_1')}</p>
-              <p className="mb-4">{t('carrots_additional_guidelines_2')}</p>
-              <p className="mb-4">{t('carrots_additional_guidelines_3')}</p>
-              <p className="mb-4">{t('carrots_additional_guidelines_4')}</p>
-              <p className="mb-4">{t('carrots_additional_guidelines_5')}</p>
-              <p className="mb-4">{t('carrots_additional_guidelines_6')}</p>
-              <p className="mb-4">{t('carrots_additional_guidelines_7')}</p>
+              <p className="mb-4">
+                {t('carrots_additional_guidelines_1')}
+              </p>
+              <p className="mb-4">
+                {t('carrots_additional_guidelines_2')}
+              </p>
+              <p className="mb-4">
+                {t('carrots_additional_guidelines_3')}
+              </p>
+              <p className="mb-4">
+                {t('carrots_additional_guidelines_4')}
+              </p>
+              <p className="mb-4">
+                {t('carrots_additional_guidelines_5')}
+              </p>
+              <p className="mb-4">
+                {t('carrots_additional_guidelines_6')}
+              </p>
+              <p className="mb-4">
+                {t('carrots_additional_guidelines_7')}
+              </p>
               <p className="mb-4">
                 {APP_NAME &&
                   APP_NAME.toLowerCase() !== 'moos' &&
@@ -158,11 +183,17 @@ CreditsPage.getInitialProps = async (context: NextPageContext) => {
       context?.locale,
       process.env.NEXT_PUBLIC_APP_NAME,
     );
+    const generalRes = await api.get('/config/general').catch(() => null);
+    const generalConfig = generalRes?.data?.results?.value;
+
     return {
+      generalConfig,
       messages,
     };
   } catch (err: unknown) {
     return {
+      generalConfig: null,
+      error: parseMessageFromError(err),
       messages: null,
     };
   }
