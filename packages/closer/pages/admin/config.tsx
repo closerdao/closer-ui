7b5c878ae089f62ccbdab2a7889fe5b661e36995
VV <vashnev@gmail.com>
2024-08-30 23:37:37 +0500

    feat: configs validation

--- packages/closer/pages/admin/config.tsx
+++ packages/closer/pages/admin/config.tsx
@@ -1,16 +1,23 @@
 import Head from 'next/head';
 
-import { ChangeEvent, useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useMemo, useState } from 'react';
 
 import ArrayConfig from '../../components/ArrayConfig';
 import PlatformFeatureSelector from '../../components/PlatformConfig/PlatformFeatureSelector';
-import { Button, Card, Heading, Information } from '../../components/ui';
+import {
+  Button,
+  Card,
+  ErrorMessage,
+  Heading,
+  Information,
+} from '../../components/ui';
 import Switcher from '../../components/ui/Switcher';
 
 import { NextPageContext } from 'next';
 import { useTranslations } from 'next-intl';
 
 import { configDescription } from '../../config';
+import { getValidationSchema } from '../../constants/validation.constants';
 import { useAuth } from '../../contexts/auth';
 import { usePlatform } from '../../contexts/platform';
 import { useConfig } from '../../hooks/useConfig';
@@ -54,6 +61,27 @@ const ConfigPage = ({ defaultEmailsConfig, error }: Props) => {
   const [hasConfigUpdated, setHasConfigUpdated] = useState(false);
   const [enabledConfigs, setEnabledConfigs] = useState<string[]>([]);
   const [isGeneralConfigEnabled, setIsGeneralConfigEnabled] = useState(false);
+  const [errors, setErrors] = useState<{
+    [key: string]: string | null | undefined | any;
+  }>({});
+
+  const currentConfig = useMemo(
+    () => updatedConfigs.find((config) => config.slug === selectedConfig),
+    [updatedConfigs, selectedConfig],
+  );
+
+  const arrayConfigsSchema =
+    updatedConfigs
+      .filter((config) => config.value.elements)
+      .map((config) => {
+        if (!Array.isArray(config.value.elements)) {
+          return;
+        }
+        const elements = config.value.elements;
+        return { len: elements.length, names: Object.keys(elements[0]) };
+      }) || [];
+
+  const configFormSchema = getValidationSchema(arrayConfigsSchema as any);
 
   useEffect(() => {
     loadData();
@@ -197,7 +225,24 @@ const ConfigPage = ({ defaultEmailsConfig, error }: Props) => {
     key = '',
     index: null | number = null,
   ) => {
-    const { name, value: inputValue } = event.target;
+    const { value: inputValue } = event.target;
+    const { name } = event.target;
+
+    const validationResult = configFormSchema?.safeParse({
+      [name]: inputValue,
+    });
+
+    if (validationResult?.success) {
+      setErrors((prevErrors) => ({ ...prevErrors, [name]: null }));
+    } else {
+      const fieldErrors: Record<string, string[] | undefined> | undefined =
+        validationResult?.error.flatten().fieldErrors;
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        [name]: fieldErrors?.[name]?.[0] || null,
+      }));
+    }
+    const strippedName = name.replace(/-?\d+$/, '');
 
     const newConfigs = [
       ...updatedConfigs.map((config) => {
@@ -212,7 +257,7 @@ const ConfigPage = ({ defaultEmailsConfig, error }: Props) => {
             const updatedArray = getUpdatedArray(
               valueToUpdate,
               index,
-              name,
+              strippedName,
               preparedInputValue,
             );
             return {
@@ -223,7 +268,7 @@ const ConfigPage = ({ defaultEmailsConfig, error }: Props) => {
 
           return {
             ...config,
-            value: { ...config.value, [name]: preparedInputValue },
+            value: { ...config.value, [strippedName]: preparedInputValue },
           };
         }
         return config;
@@ -350,6 +395,7 @@ const ConfigPage = ({ defaultEmailsConfig, error }: Props) => {
                 return (
                   <div key={key} className="flex flex-col gap-1">
                     <label>{t(`config_label_${key}`)}:</label>
+
                     {!isSelect && (
                       <input
                         className="bg-neutral rounded-md p-1"
@@ -401,132 +447,130 @@ const ConfigPage = ({ defaultEmailsConfig, error }: Props) => {
             />
 
             <Card className="flex flex-col gap-10">
-              {updatedConfigs &&
-                updatedConfigs.map((config: Config) => {
-                  if (selectedConfig === config.slug && config.value.enabled) {
+              {currentConfig && (
+                <div
+                  key={`${currentConfig.slug}`}
+                  className="flex flex-col gap-4"
+                >
+                  <Heading level={2}>
+                    {capitalizeFirstLetter(currentConfig.slug)}
+                  </Heading>
+
+                  {Object.entries(currentConfig.value).map(([key, value]) => {
+                    const currentValue = currentConfig.value[key];
+
+                    const description = mergedConfigDescription?.find(
+                      (c) => c.slug === currentConfig.slug,
+                    )?.value as Record<string, any>;
+                    const inputType = description?.[key]?.type;
+                    const isArray = Array.isArray(inputType);
+                    const isSelect = inputType === 'select';
+                    const selectOptions = description?.[key]?.enum;
+
+                    if (key === 'enabled') {
+                      return null;
+                    }
+
                     return (
-                      <div
-                        key={`${config.slug}`}
-                        className="flex flex-col gap-4"
-                      >
-                        <Heading level={2}>
-                          {capitalizeFirstLetter(config.slug)}
-                        </Heading>
+                      <>
+                        {key !== 'enabled' && (
+                          <div
+                            key={`${currentConfig.slug}-${key}`}
+                            className="flex flex-col gap-1"
+                          >
+                            {!isArray && (
+                              <label>{t(`config_label_${key}`)}:</label>
+                            )}
+                            {typeof value === 'boolean' ? (
+                              <div className="flex gap-3">
+                                <label className="flex gap-1 items-center">
+                                  <input
+                                    type="radio"
+                                    name={key}
+                                    value="true"
+                                    checked={currentValue === true}
+                                    onChange={handleChange}
+                                  />
+                                  {t('config_true')}
+                                </label>
+                                <label className="flex gap-1 items-center">
+                                  <input
+                                    type="radio"
+                                    name={key}
+                                    value="false"
+                                    checked={currentValue === false}
+                                    onChange={handleChange}
+                                  />
+                                  {t('config_false')}
+                                </label>
+                              </div>
+                            ) : (
+                              <div>
+                                {isArray && (
+                                  <div>
+                                    <ArrayConfig
+                                      currentValue={currentValue}
+                                      handleChange={handleChange}
+                                      handleAddElement={handleAddElement}
+                                      handleDeleteElement={handleDeleteElement}
+                                      elementsKey={key}
+                                      description={description}
+                                      slug={currentConfig.slug}
+                                      resetToDefault={resetToDefault}
+                                      errors={errors}
+                                    />
+                                  </div>
+                                )}
+                                {!isArray && !isSelect && (
+                                  <input
+                                    className="bg-neutral rounded-md p-1"
+                                    name={key}
+                                    onChange={handleChange}
+                                    type="text"
+                                    value={String(currentValue)}
+                                  />
+                                )}
 
-                        {Object.entries(config.value).map(([key, value]) => {
-                          const currentValue:
-                            | string
-                            | number
-                            | boolean
-                            | any[] =
-                            updatedConfigs.find(
-                              (config) => config.slug === selectedConfig,
-                            )?.value[key] ?? [];
+                                {errors[key] && (
+                                  <ErrorMessage
+                                    error={errors[key].toString()}
+                                  ></ErrorMessage>
+                                )}
 
-                          const description = mergedConfigDescription?.find(
-                            (c) => c.slug === config.slug,
-                          )?.value as Record<string, any>;
-                          const inputType = description?.[key]?.type;
-                          const isArray = Array.isArray(inputType);
-                          const isSelect = inputType === 'select';
-                          const selectOptions = description?.[key]?.enum;
-
-                          return (
-                            <>
-                              {key !== 'enabled' && (
-                                <div key={key} className="flex flex-col gap-1">
-                                  {!isArray && (
-                                    <label>{t(`config_label_${key}`)}:</label>
-                                  )}
-
-                                  {typeof value === 'boolean' ? (
-                                    <div className="flex gap-3">
-                                      <label className="flex gap-1 items-center">
-                                        <input
-                                          type="radio"
-                                          name={key}
-                                          value="true"
-                                          checked={currentValue === true}
-                                          onChange={handleChange}
-                                        />
-                                        {t('config_true')}
-                                      </label>
-                                      <label className="flex gap-1 items-center">
-                                        <input
-                                          type="radio"
-                                          name={key}
-                                          value="false"
-                                          checked={currentValue === false}
-                                          onChange={handleChange}
-                                        />
-                                        {t('config_false')}
-                                      </label>
-                                    </div>
-                                  ) : (
-                                    <div>
-                                      {isArray && (
-                                        <div>
-                                          <ArrayConfig
-                                            currentValue={currentValue}
-                                            handleChange={handleChange}
-                                            handleAddElement={handleAddElement}
-                                            handleDeleteElement={
-                                              handleDeleteElement
-                                            }
-                                            elementsKey={key}
-                                            description={description}
-                                            slug={config.slug}
-                                            resetToDefault={resetToDefault}
-                                          />
-                                        </div>
-                                      )}
-                                      {!isArray && !isSelect && (
-                                        <input
-                                          className="bg-neutral rounded-md p-1"
-                                          name={key}
-                                          onChange={handleChange}
-                                          type="text"
-                                          value={String(currentValue)}
-                                        />
-                                      )}
-
-                                      {isSelect && (
-                                        <select
-                                          className="px-2 py-1"
-                                          value={String(currentValue)}
-                                          onChange={handleChange}
-                                          name={key}
-                                        >
-                                          {selectOptions.map(
-                                            (option: string) => {
-                                              return (
-                                                <option
-                                                  value={option}
-                                                  key={option}
-                                                >
-                                                  {option}
-                                                </option>
-                                              );
-                                            },
-                                          )}
-                                        </select>
-                                      )}
-                                    </div>
-                                  )}
-                                </div>
-                              )}
-                            </>
-                          );
-                        })}
-                      </div>
+                                {isSelect && (
+                                  <select
+                                    className="px-2 py-1"
+                                    value={String(currentValue)}
+                                    onChange={handleChange}
+                                    name={key}
+                                  >
+                                    {selectOptions.map((option: string) => {
+                                      return (
+                                        <option value={option} key={option}>
+                                          {option}
+                                        </option>
+                                      );
+                                    })}
+                                  </select>
+                                )}
+                              </div>
+                            )}
+                          </div>
+                        )}
+                      </>
                     );
-                  }
-                })}
+                  })}
+                </div>
+              )}
 
               <Button
                 onClick={handleSaveConfig}
                 isLoading={isLoading}
-                isEnabled={!isLoading}
+                isEnabled={
+                  !isLoading &&
+                  errors &&
+                  Object.values(errors).every((value) => value === null)
+                }
               >
                 {t('generic_save_button')}
               </Button>
