b23c0cc64d6ead5545b5afb1dc4da9d0c488b890
VV <vashnev@gmail.com>
2024-09-27 16:05:39 +0500

    hotfix: event creation with food

--- packages/closer/pages/events/create.tsx
+++ packages/closer/pages/events/create.tsx
@@ -4,15 +4,32 @@ import { useRouter } from 'next/router';
 import EditModel from '../../components/EditModel';
 import Heading from '../../components/ui/Heading';
 
-import { NextPageContext } from 'next';
+import { NextApiRequest, NextPageContext } from 'next';
 import { useTranslations } from 'next-intl';
 
 import models from '../../models';
 import { loadLocaleData } from '../../utils/locale.helpers';
+import { FoodOption } from '../../types/food';
+import api from '../../utils/api';
 
-const CreateEvent = () => {
+interface Props {
+  foodOptions: FoodOption[];
+}
+
+const CreateEvent = ({ foodOptions }: Props) => {
   const t = useTranslations();
   const router = useRouter();
+
+  const foodOptionsWithDefault = [
+    {
+      label: 'Allow guests to select',
+      value: '',
+    },
+    ...foodOptions?.map((option) => ({
+      label: option.name,
+      value: option._id,
+    })),
+  ];
 
   return (
     <>
@@ -24,6 +41,10 @@ const CreateEvent = () => {
           {t('events_create_title')}
         </Heading>
         <EditModel
+          dynamicField={{
+            name: 'foodOptionId',
+            options: foodOptionsWithDefault,
+          }}
           endpoint={'/event'}
           fields={models.event}
           onSave={(event) => router.push(`/events/${event.slug}`)}
@@ -35,16 +56,24 @@ const CreateEvent = () => {
 
 CreateEvent.getInitialProps = async (context: NextPageContext) => {
   try {
-    const messages = await loadLocaleData(
-      context?.locale,
-      process.env.NEXT_PUBLIC_APP_NAME,
-    );
+    const [foodRes, messages] = await Promise.all([
+
+      api.get('/food').catch((err) => {
+        console.error('Error fetching food:', err);
+        return null;
+      }),
+      loadLocaleData(context?.locale, process.env.NEXT_PUBLIC_APP_NAME),
+    ]);
+
+    const foodOptions = foodRes?.data?.results;
     return {
       messages,
+      foodOptions,
     };
   } catch (err: unknown) {
     return {
       messages: null,
+      foodOptions: null,
     };
   }
 };
