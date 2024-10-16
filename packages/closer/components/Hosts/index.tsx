4dfbb5bf36890ab1663eecf7a7b66b2e71231114
VV <vashnev@gmail.com>
2024-10-16 19:49:01 +0500

    hotfix: hide hosts intro text on tdf

--- packages/closer/components/Hosts/index.tsx
+++ packages/closer/components/Hosts/index.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link';
 
 import { useTranslations } from 'next-intl';
 
+import { useConfig } from '../../hooks/useConfig';
 import UserPreview from '../UserPreview';
 import { Heading } from '../ui';
-
 
 interface Props {
   hosts: any;
@@ -12,14 +12,17 @@ interface Props {
 }
 
 const Hosts = ({ hosts, email }: Props) => {
-  const t = useTranslations(); 
+  const t = useTranslations();
+  const { APP_NAME } = useConfig();
 
   // Move Agnes from Lios to the end of the space hosts list
-  const sortedHosts = hosts && hosts.sort((a: Record<string, any>, b: Record<string, any>) => {
-    if (a.get('slug').includes('agnese')) return 1;
-    if (b.get('slug').includes('agnese')) return -1;
-    return 0;
-  });
+  const sortedHosts =
+    hosts &&
+    hosts.sort((a: Record<string, any>, b: Record<string, any>) => {
+      if (a.get('slug').includes('agnese')) return 1;
+      if (b.get('slug').includes('agnese')) return -1;
+      return 0;
+    });
 
   return (
     <div className="mb-16">
@@ -27,13 +30,13 @@ const Hosts = ({ hosts, email }: Props) => {
         <Heading level={2} className="text-2xl pb-2 my-8">
           {t('stay_meet_your_hosts')}
         </Heading>
-        {!t('stay_meet_your_hosts_description').includes('missing') && (
+        {APP_NAME !== 'tdf' && (
           <p className="mb-8">{t('stay_meet_your_hosts_description')}</p>
         )}
       </div>
       {hosts && hosts.count() > 0 && (
         <div className="grid md:grid-cols-3 gap-x-4 gap-y-4">
-          {(sortedHosts).map((host: any) => {
+          {sortedHosts.map((host: any) => {
             return <UserPreview key={host.get('_id')} user={host} />;
           })}
         </div>
