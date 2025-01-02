93964143736d2c3af68cf7642cf46968d2f48bf4
Sam <closer.earth@protonmail.com>
2025-01-02 21:23:40 +0000

    Add 2024 report & links

--- apps/tdf/pages/index.tsx
+++ apps/tdf/pages/index.tsx
@@ -302,6 +302,13 @@ const HomePage = () => {
             >
               2022 report
             </Link>
+            <Link
+              href="/pdf/2024-TDF-report.pdf"
+              target="_blank"
+              className="bg-accent-light px-5 py-2 rounded-full uppercase"
+            >
+              2024 report
+            </Link>
           </div>
         </section>
 
