1235edcb1e09ab81eea3b2074bc683cd2546fd8e
VV <vashnev@gmail.com>
2025-04-26 17:56:51 +0500

    feat: combine booking disclaimer conditions

--- packages/closer/components/Conditions.tsx
+++ packages/closer/components/Conditions.tsx
@@ -1,10 +1,8 @@
-import { useEffect, useState } from 'react';
-import { MouseEvent } from 'react';
+import { MouseEvent, useEffect, useState } from 'react';
 
 import { useTranslations } from 'next-intl';
 import PropTypes from 'prop-types';
 
-import { useAuth } from '../contexts/auth';
 import Checkbox from './Checkbox';
 import Modal from './Modal';
 import Heading from './ui/Heading';
@@ -27,21 +25,16 @@ const Conditions = ({
 }: Props) => {
   const t = useTranslations();
 
-  const { user } = useAuth();
-  const isMember = user?.roles.includes('member');
-
-  const [isVisitorsGuideChecked, setIsVisitorsGuideChecked] = useState(false);
-  const [isCancellationPolicyChecked, setIsCancellationPolicyChecked] =
-    useState(false);
+  const [doesUserAcceptTerms, setDoesUserAcceptTerms] = useState(false);
   const [isInfoModalOpened, setIsInfoModalOpened] = useState(false);
 
   useEffect(() => {
-    if (isVisitorsGuideChecked && isCancellationPolicyChecked) {
+    if (doesUserAcceptTerms) {
       setComply(true);
     } else {
       setComply(false);
     }
-  }, [isVisitorsGuideChecked, isCancellationPolicyChecked]);
+  }, [doesUserAcceptTerms]);
 
   const stopPropagation = (event: MouseEvent<HTMLDivElement>) => {
     event.stopPropagation();
@@ -60,8 +53,8 @@ const Conditions = ({
   return (
     <div className="mt-8" onClick={stopPropagation}>
       <Checkbox
-        checked={isVisitorsGuideChecked}
-        onChange={() => setIsVisitorsGuideChecked(!isVisitorsGuideChecked)}
+        checked={doesUserAcceptTerms}
+        onChange={() => setDoesUserAcceptTerms(!doesUserAcceptTerms)}
         className="mb-4"
       >
         <p>
@@ -74,16 +67,7 @@ const Conditions = ({
           >
             {t('bookings_checkout_step_visitors_guide')}
           </a>
-        </p>
-      </Checkbox>
-      <Checkbox
-        checked={isCancellationPolicyChecked}
-        onChange={() =>
-          setIsCancellationPolicyChecked(!isCancellationPolicyChecked)
-        }
-      >
-        <p>
-          <span>{t('bookings_checkout_step_comply_with')}</span>
+          <span className="mx-2">{t('generic_and')}</span>
           <button
             className="border-b pb-1 border-neutral-400 border-dashed"
             onClick={openModal}
@@ -92,6 +76,7 @@ const Conditions = ({
           </button>
         </p>
       </Checkbox>
+
       {isInfoModalOpened && (
         <Modal closeModal={closeModal}>
           <div>
