f7c4434b8f8d9d2a0a5944924dbab8e49779e090
VV <vashnev@gmail.com>
2025-07-18 12:14:41 +0500

    feat: edit user prefs on booking flow

--- packages/closer/pages/settings/index.tsx
+++ packages/closer/pages/settings/index.tsx
@@ -10,6 +10,7 @@ import Heading from '../../components/ui/Heading';
 import Input from '../../components/ui/Input';
 import Select from '../../components/ui/Select/Dropdown';
 import MultiSelect from '../../components/ui/Select/MultiSelect';
+import { SHARED_ACCOMMODATION_PREFERENCES } from '../../constants/shared.constants';
 
 import { NextPageContext } from 'next';
 import { useTranslations } from 'next-intl';
@@ -206,11 +207,7 @@ const DeleteAccountSection = ({ t }: DeleteAccountSectionProps) => {
   );
 };
 
-const SHARED_ACCOMODATION_PREFERENCES = [
-  { label: 'Flexible', value: 'flexible' },
-  { label: 'Male Only', value: 'male only' },
-  { label: 'Female Only', value: 'female only' },
-];
+
 
 const SettingsPage = ({
   volunteerConfig,
@@ -614,7 +611,7 @@ const SettingsPage = ({
                     <Select
                       label={t('settings_shared_accommodation_preference')}
                       value={user?.preferences?.sharedAccomodation}
-                      options={SHARED_ACCOMODATION_PREFERENCES}
+                      options={SHARED_ACCOMMODATION_PREFERENCES}
                       className="mb-4"
                       onChange={saveUserData('sharedAccomodation')}
                       isRequired
