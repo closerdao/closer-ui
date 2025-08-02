9b3619f686529b8e181906714afd1f94e9f81164
Closer <closer.earth@protonmail.com>
2025-08-02 15:43:03 +0100

    Set min, max, and horizon for all and guests (#742)
    * Set min, max, and horizon for all and guests
    
    * Update min stay message for members
    
    * Add missing memberMaxBookingHorizon

--- packages/closer/types/api.ts
+++ packages/closer/types/api.ts
@@ -3,10 +3,12 @@ import { User } from 'closer/contexts/auth/types';
 import { CloserCurrencies, Price } from './currency';
 
 export type BookingConditions = {
+  minDuration: number | undefined;
+  maxDuration: number | undefined;
+  maxBookingHorizon: number | undefined;
+  memberMinDuration: number | undefined;
   memberMaxDuration: number | undefined;
   memberMaxBookingHorizon: number | undefined;
-  guestMaxDuration: number | undefined;
-  guestMaxBookingHorizon: number | undefined;
 };
 
 export type VolunteerOpportunity = {
@@ -54,11 +56,11 @@ export type BookingSettings = {
   checkoutTime: number;
   maxDuration: number;
   minDuration: number;
+  maxBookingHorizon: number;
   volunteerCommitment: string;
+  memberMinDuration: number;
   memberMaxDuration: number;
   memberMaxBookingHorizon: number;
-  guestMaxDuration: number;
-  guestMaxBookingHorizon: number;
   discountsDaily: number;
   discountsWeekly: number;
   discountsMonthly: number;
@@ -128,15 +130,17 @@ export type AffiliateConfig = {
 };
 
 export type BookingConfig = {
-  guestMaxDuration: number;
   enabled: boolean;
   minDuration: number;
+  maxDuration: number;
+  maxBookingHorizon: number;
+  memberMinDuration: number;
+  memberMaxDuration: number;
+  memberMaxBookingHorizon: number;
   discountsDaily: number;
   seasonsHighModifier: number;
   seasonsHighEnd: string;
   cancellationPolicyDefault: number;
-  memberMaxBookingHorizon: number;
-  guestMaxBookingHorizon: number;
   seasonsHighStart: string;
   cancellationPolicyLastday: number;
   checkoutTime: number;
@@ -144,11 +148,9 @@ export type BookingConfig = {
   utilityTokenCur: string;
   utilityTokenVal: number;
   cancellationPolicyLastmonth: number;
-  maxDuration: number;
   checkinTime: number;
   discountsMonthly: number;
   utilityDayFiatVal: number;
-  memberMaxDuration: number;
   utilityFiatCur: string;
   volunteerCommitment: string;
   cancellationPolicyLastweek: number;
