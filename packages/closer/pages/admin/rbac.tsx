19cc305aceeeef09e71a6d7bf1fe6c9a8ce20b2f
VV <vashnev@gmail.com>
2025-08-03 13:28:39 +0500

    fix: roles page update tweaks

--- packages/closer/pages/admin/rbac.tsx
+++ packages/closer/pages/admin/rbac.tsx
@@ -63,6 +63,9 @@ const RBACPage = ({ loadConfig, bookingConfig }: Props) => {
     'Token',
     'Blog',
     'TokenSales',
+    'RoleCreation',
+    'Roles',
+
   ]);
   const [roles] = useState([
     'default', // Add default as a "role" for the UI
