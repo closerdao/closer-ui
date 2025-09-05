18809efffd69d0082adb2dfb585fcee73c764c5e
Vladimir <vashnev@gmail.com>
2025-09-05 18:00:43 +0500

    fix: quotes ts issue

--- packages/closer/components/custom-pages/CustomVideoEmbed.tsx
+++ packages/closer/components/custom-pages/CustomVideoEmbed.tsx
@@ -7,8 +7,8 @@ const CustomVideoEmbed: React.FC<{
     embedId: string;
   };
 }> = ({ content }) => (
-  <section className={`w-full flex  justify-center  text-md`}>
-    <div className='w-full max-w-4xl  aspect-video mx-auto'>
+  <section className={'w-full flex  justify-center  text-md'}>
+    <div className="w-full max-w-4xl  aspect-video mx-auto">
       {content.embedId && <GenericYoutubeEmbed embedId={content.embedId} />}
     </div>
   </section>
