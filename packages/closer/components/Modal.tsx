cab3e256d79aa665d013f427080dc6d472fc5271
Sam <closer.earth@protonmail.com>
2026-04-13 22:04:18 +0200

    Fix governance comments

--- packages/closer/components/Modal.tsx
+++ packages/closer/components/Modal.tsx
@@ -14,25 +14,25 @@ interface Props {
 
 const Modal = ({ children, closeModal, doesShowVideo, className }: Props) => {
   return (
-    <div className="fixed inset-0 z-[100] flex items-center justify-center min-h-[600px]">
+    <div className="fixed inset-0 z-[100] flex items-end sm:items-center justify-center">
       <button className="fixed inset-0 bg-black opacity-50" onClick={closeModal} />
       <div
-        className={`p-5 rounded-md relative bg-white z-[101] shadow-lg   ${twMerge(
-          ' w-full h-full md:h-2/3 sm:p-8 md:w-[500px]',
+        className={`p-4 rounded-t-xl sm:rounded-md relative bg-white z-[101] shadow-lg max-h-[92vh] sm:max-h-[85vh] overflow-y-auto ${twMerge(
+          'w-full sm:p-8 md:w-[500px]',
           className,
           doesShowVideo ? 'pt-20 sm:pt-20 md:w-[850px] md:h-[600px]' : '',
-        )} `}
+        )}`}
       >
         <button
-          className="absolute top-4 right-4 text-2xl"
+          className="absolute top-3 right-3 sm:top-4 sm:right-4 text-2xl z-10"
           onClick={closeModal}
         >
           <CloseIcon />
         </button>
 
         <div
-          className={`flex flex-col   ${twMerge(
-            'h-full',
+          className={`flex flex-col ${twMerge(
+            '',
             doesShowVideo ? 'h-[300px] sm:h-[450px]' : '',
           )}`}
         >
